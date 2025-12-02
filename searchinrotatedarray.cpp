ece6126b82b61da3a2c6550f7d9f0afee8c6e51f
Rishita Chakravorty <chakravorty.r20@outlook.com>
2025-12-02 21:37:16 +0530

    binary search logic in rotated sorted array

--- searchinrotatedarray.cpp
+++ searchinrotatedarray.cpp
@@ -12,27 +12,27 @@ int binarysearchapproach(vector<int> nums, int target) {
     int low = 0, high = nums.size() - 1;
     while(low <= high) {
         int mid = (low + high) / 2;
-        if(nums[mid] == target) return mid + 1;
+        if(nums[mid] == target) return mid;
         
-        if(nums[low] > nums[mid]) {
-            if(nums[mid] < target && nums[high] > target) {
-                low = mid + 1;
-            } else {
+        if(nums[low] <= nums[mid]) {
+            if(nums[mid] >= target && nums[low] <= target) {
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         } else {
-            if(nums[mid] > target && nums[low] <= target) {
-                high = mid - 1;
-            } else {
+            if(nums[mid] < target && nums[low] >= target) {
                 low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
     }
     return -1;
 }
 int main() {
-    vector<int> nums = {6,7,0,1,2,3,4,5};
-    int target = 1;
+    vector<int> nums = {5,1,3};
+    int target = 3;
     cout << binarysearchapproach(nums, target);
     return 0;
 }
