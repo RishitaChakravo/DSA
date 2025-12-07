ffe18b181dc8c6e2fc167adea70c2e5d684ecf08
Rishita Chakravorty <chakravorty.r20@outlook.com>
2025-12-07 16:34:14 +0530

    singleelement

--- singleelement.cpp
+++ singleelement.cpp
@@ -5,19 +5,27 @@ using namespace std;
 
 int binarysearch(vector<int> nums) {
     int n = nums.size();
-    if(nums[0] != nums[1]) return 0;
-    if(nums[n-1] != nums[n-2]) return n-1;
+    if(n==1) return nums[0];
+    if(nums[0] != nums[1]) return nums[0];
+    if(nums[n-1] != nums[n-2]) return nums[n-1];
+    
     int low = 1, high = nums.size() - 2;
-    int ans = 0;
+    
     while(low <= high) {
         int mid = (low + high) / 2;
-        if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return mid;
-        
+
+        if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid+1]) return nums[mid];
+
+        if((mid%2 == 0 && nums[mid] == nums[mid+1]) || (mid%2!=0 && nums[mid] == nums[mid-1])) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
-    return ans;
+    return -1;
 }
 int main() {
-    vector<int> nums = {1,1,2,2,2,3,3,4,5,5,5,6,6};
+    vector<int> nums = {1,1,2,3,3,4,4,8,8};
     cout << binarysearch(nums);
     return 0;
 }
