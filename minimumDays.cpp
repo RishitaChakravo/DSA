05142b48d401b54a1ebc1b3a9158c60f6a8ade55
Rishita Chakravorty <chakravorty.r20@outlook.com>
2026-01-02 11:48:40 +0530

    mindays-binary

--- minimumDays.cpp
+++ minimumDays.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <climits>
+#include <algorithm>
 #define N 100
 using namespace std;
 
 // minimum number of days to make m bouquets each with k roses
+// this does work for a number of cases but not for all cases
+// here the number of days arent taken into consideration and this overall just takes care of certain test cases storing vectors which is not resouce effective
 int bouquets(vector<int> arr, int k, int m) {
     if(k * m > arr.size()) return -1;
     map<int, vector<vector<int>>> bouquet;
@@ -29,10 +33,68 @@ int bouquets(vector<int> arr, int k, int m) {
     return it->first;
 }
 // only checks of flowers are adjacent 🥲 we also need it to check if the elements are either equal to or smaller than the prev element
+// BRUTE FORCE
+int mindays_brute(vector<int> arr, int k, int m) {
+    int bloomsDay = INT_MAX;
+    for(int i = 0; i < arr.size(); i++) {
+        int D = arr[i];
+        int bouquet = 0;
+        int flowers = 0;
 
+        for(int j = 0; j < arr.size(); j++) {
+            if(arr[j] <= D) {
+                flowers++;
+                if(flowers == k) {
+                    bouquet++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+
+        if(bouquet >= m) {
+            bloomsDay = min(bloomsDay, D);
+        }
+    }
+    return (bloomsDay == INT_MAX ? -1 : bloomsDay);
+}
+
+int mindays_binary(vector<int> arr, int k, int m) {
+    if ((long long)m * k > arr.size()) return -1;
+    int bloomsday = -1;
+    int low = *min_element(arr.begin(), arr.end());
+    int high = *max_element(arr.begin(), arr.end());
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        int bouquets = 0;
+        int flowers = 0;
+        int D = mid;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] <= D) {
+                flowers++;
+                if(flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+
+        if(bouquets >= m) {
+            bloomsday = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return bloomsday;
+}
 int main() {
-    vector<int> nums = {3, 1, 3, 1, 3};
-    int m = 1, k = 3;
-    cout << bouquets(nums, k, m);
+    vector<int> nums = {7,7,7,7,7,13,11,12,7};
+    int m = 2, k = 3;
+    cout << mindays_binary(nums, k, m);
     return 0;
 }
