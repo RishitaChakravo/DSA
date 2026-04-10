e05ab1aa6d5928d99bb7086e2929ca718b4bfe6d
Rishita Chakravorty <chakravorty.r20@outlook.com>
2026-04-10 18:58:37 +0530

    10-04-26

---
+++ BitManipulation/CheckIfPowerof2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+bool checkIfPower2(int n) {
+    return (n & (n - 1)) == 0;
+}
+int main() {
+
+    return 0;
+}
